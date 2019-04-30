9023382fe40f1fb7a662d9f4bc87c92169f7f86d
Brian Gill <work.briangill@gmail.com>
2019-04-29 23:22:24 -0700

    Make the program file
    oh man this sucks. I had file with a working rgb function but then windows went kamikaze and I lost it, so this is my 5 minute recreation that obviosly doesn't work. All the includes defines and what not were taken from the ili9341 graphicstest or something similar to that name, then I stripped it of anything I didn't need, and started building my program off of that.

---
+++ program/program.ino
@@ -0,0 +1,70 @@
+using namespace std;
+#include "SPI.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
+
+#define TFT_DC 9
+#define TFT_CS 10
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
+void setup() {
+  Serial.begin(9600);
+  tft.begin();
+}
+
+long rgb(int red, int green, int blue) {
+  int bin[16], i;
+  long number, tRed, tGreen, tBlue;
+
+  tRed = red * 31 / 255;
+  tGreen = green * 63 / 255;
+  tBlue = blue * 31 / 255;
+
+  tRed = static_cast<int>(tRed);
+  tGreen = static_cast<int>(tGreen);
+  tBlue = static_cast<int>(tBlue);
+
+  for (i=0; i<16; i++) {
+    if (i<5) {
+      bin[i] = tBlue % 2;
+      tBlue = tBlue / 2;
+    }
+
+    else if (i<11) {
+      bin[i] = tGreen % 2;
+      tGreen = tGreen / 2;
+    }
+
+    else {
+      bin[i] = tRed % 2;
+      tRed = tRed / 2;
+    }
+  }
+
+  number = 0;
+
+  for (i=0; i<16; i++) {
+    Serial.println(number);
+    number = number + pow(bin[i]*2, i);
+  }
+
+  return number;
+}
+
+void loop(void) {
+  test();
+}
+
+unsigned long test() {
+  tft.fillScreen(rgb(255, 255, 255));
+  delay(1000);
+  tft.fillScreen(rgb(255, 0, 0));
+  delay(1000);
+  tft.fillScreen(rgb(0, 255, 0));
+  delay(1000);
+  tft.fillScreen(rgb(0, 0, 255));
+  delay(1000);
+  tft.fillScreen(rgb(0, 0, 0));
+  delay(1000);
+}
