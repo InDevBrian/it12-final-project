94989cc4dd0e94062846a3a9284e396009b8be12
Brian Gill <work.briangill@gmail.com>
2019-06-01 21:04:06 -0700

    Pretty large refactoring
    now the snake is kept in a 2 dimensional array, the head is the first, and tail is all the rest. There is still a problem with the input. It is extremely inconsistent and largley doesn't work. I do not know why

--- program/snake.h
+++ program/snake.h
@@ -1,21 +1,18 @@
-#include "functions.h"
-
 int board [15][20];
 int boardDirections [15][20];
+int snakeBody [15][2] = { {8, 8}, {8, 9}, {8, 10} };
 
-int headX = 8;
-int headY = 8;
 int snakeDirection = 0;
 
 unsigned long snakeRender() {
   for ( int x = 0; x < 15; x++ ) {
     for ( int y = 0; y < 20; y++ ) {
-      if (::board[x][y] == 0) {
+      if (board[x][y] == 0) {
         tft.fillRect(x*16+1, y*16+1, 14, 14, 0xFFFF);
-      } else if ( ::board[x][y] == 1 ) {
+      } else if ( board[x][y] == 1 ) {
         tft.fillRect(x*16+1, y*16+1, 14, 14, 0xF000);
       } else {
-        tft.fillRect(x*16+1, y*16+1, 14, 14, rgb(0, 255, 0);
+        tft.fillRect(x*16+1, y*16+1, 14, 14, rgb(0, 255, 0));
       }
     }
   }
@@ -27,50 +24,47 @@ unsigned long snake() {
   int left = analogRead(2);
   int right = analogRead(3);
 
-  ::board[::headX][::headY] = 0;
+  Serial.println(snakeDirection);
 
-  if ( up == 1022 ) {
+  board[snakeBody[0][0]][snakeBody[0][1]] = 0;
+
+  if ( (up == 1022) && (snakeDirection != 2) ) {
+    Serial.println("Up");
     snakeDirection = 0;
-  } else if ( right == 1022 ) {
+  } else if ( (right == 1022) && (snakeDirection != 3) ) {
+    Serial.println("Right");
     snakeDirection = 1;
-  } else if ( down == 1022 ) {
+  } else if ( (down == 1022) && (snakeDirection != 0) ) {
+    Serial.println("Down");
     snakeDirection = 2;
-  } else if ( left == 1022 ) {
+  } else if ( (left == 1022) && (snakeDirection != 1) ) {
+    Serial.println("Left");
     snakeDirection = 3;
   }
 
+  boardDirections[snakeBody[0][0]][snakeBody[0][1]] = snakeDirection;
+
   switch( snakeDirection ) {
-      case 0 : ::headY = ::headY - 1; break;
-      case 1 : ::headX = ::headX + 1; break;
-      case 2 : ::headY = ::headY + 1; break;
-      case 3 : ::headX = ::headX - 1; break;
+    case 0 : snakeBody[0][1] = snakeBody[0][1] - 1; break;
+    case 1 : snakeBody[0][0] = snakeBody[0][0] + 1; break;
+    case 2 : snakeBody[0][1] = snakeBody[0][1] + 1; break;
+    case 3 : snakeBody[0][0] = snakeBody[0][0] - 1; break;
   }
 
-  ::board[::headX][::headY] = 1;
+  board[snakeBody[0][0]][snakeBody[0][1]] = 1;
 
-   for ( int x = 0; x < 15; x++ ) {
-    for ( int y = 0; y < 20; y++ ) {
-      if (::board[x][y] == 2) {
-        ::board[x][y] == 0
-        newPos = snakeBody({ x, y });
-        ::board[newPos[0]][newPos[1]] = 2;
-      }
+  for (int b = 1; b < 15; b++) {
+    board[snakeBody[b][0]][snakeBody[b][1]] = 0;
+
+    switch( boardDirections[snakeBody[b][0]][snakeBody[b][1]] ) {
+      case 0 : snakeBody[b][1] = snakeBody[b][1] - 1; break;
+      case 1 : snakeBody[b][0] = snakeBody[b][0] + 1; break;
+      case 2 : snakeBody[b][1] = snakeBody[b][1] + 1; break;
+      case 3 : snakeBody[b][0] = snakeBody[b][0] - 1; break;
     }
+
+    board[snakeBody[b][0]][snakeBody[b][1]] = 2;
   }
-  
+
   snakeRender();
-}
-
-unsigned long snakeBody( int snakeBodyArray[2] ) {
-  bodyX = snakeBodyArray[0];
-  bodyY = snakeBodyArray[1];
-
-  switch( ::boardDirections[::bodyX][::bodyY] ) {
-      case 0 : bodyY = bodyY - 1; break;
-      case 1 : bodyX = bodyX + 1; break;
-      case 2 : bodyY = bodyY + 1; break;
-      case 3 : bodyX = bodyX - 1; break;
-  }
-  
-  return { bodyX, bodyY };
 }
