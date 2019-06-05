6f6017ce9be1d2e0c7c472bc897d3c841f55bf38
Brian Gill <work.briangill@gmail.com>
2019-06-05 14:22:23 -0700

    More optimisation, + general fixing everything
    those breaks were actually kinda important, now to un-render the tail, the snake will always be kept  1 piece longer, and that last piece will be rendered white. also cleared up lots of memory by switching to only rendering the snake, not needing any more board

--- program/snake.h
+++ program/snake.h
@@ -1,53 +1,40 @@
-int board [12][16];
 int boardDirections [12][16];
-int snakeBody [15][2] = { {8, 8}, {8, 9}, {8, 10} };
+int snakeBody [100][2] = { {8, 8}, {8, 9}, {8, 10} };
 
 int firstRender = 1;
 int snakeDirection = 0;
+int snakeLength = 3;
 
 unsigned long snakeRender() {
   if ( firstRender == 1 ) {
     for ( int x = 0; x < 12; x++ ) {
       for ( int y = 0; y < 16; y++ ) {
-        if (board[x][y] == 0) {
-          tft.fillRect(x*20+2, y*20+2, 16, 16, 65535);
-        } else if ( board[x][y] == 1 ) {
-          tft.fillRect(x*20+2, y*20+2, 16, 16, 61440);
-        } else {
-          tft.fillRect(x*20+2, y*20+2, 16, 16, 2016);
-        }
+        tft.fillRect(x*20+2, y*20+2, 16, 16, 65535);
       }
     }
 
     firstRender = 0;
   }
 
-  for ( int p = 0; p < 15; p++ ) {
+  tft.fillRect(snakeBody[0][0]*20+2, snakeBody[0][1]*20+2, 16, 16, 61440);
+
+  for ( int p = 1; p < snakeLength-1; p++ ) {
     int x = snakeBody[p][0];
     int y = snakeBody[p][1];
-
-    if (board[x][y] == 0) {
-      tft.fillRect(x*20+2, y*20+2, 16, 16, 65535);
-    } else if ( board[x][y] == 1 ) {
-      tft.fillRect(x*20+2, y*20+2, 16, 16, 61440);
-    } else {
-      tft.fillRect(x*20+2, y*20+2, 16, 16, 2016);
-    }
+    
+    tft.fillRect(x*20+2, y*20+2, 16, 16, 2016);
   }
+
+  tft.fillRect(snakeBody[snakeLength-1][0]*20+2, snakeBody[snakeLength-1][1]*20+2, 16, 16, 65535);
 }
 
 unsigned long snakeMove(int a, int d) {
-  board[snakeBody[a][0]][snakeBody[a][1]] = 0;
-  
   switch( d ) {
-    case 0 : snakeBody[a][1] -= 1;
-    case 1 : snakeBody[a][0] += 1;
-    case 2 : snakeBody[a][1] += 1;
-    case 3 : snakeBody[a][0] -= 1;
+    case 0 : snakeBody[a][1] -= 1; break;
+    case 1 : snakeBody[a][0] += 1; break;
+    case 2 : snakeBody[a][1] += 1; break;
+    case 3 : snakeBody[a][0] -= 1; break;
   }
-
-  if ( a == 0 ) { board[snakeBody[a][0]][snakeBody[a][1]] = 1; }
-  else { board[snakeBody[a][0]][snakeBody[a][1]] = 2; }
 }
 
 unsigned long snake() {
@@ -55,8 +42,6 @@ unsigned long snake() {
   int down = analogRead(1);
   int left = analogRead(2);
   int right = analogRead(3);
-
-  delay(300);
 
   if ( (up == 1022) && (snakeDirection != 2) ) {
     snakeDirection = 0;
@@ -68,13 +53,13 @@ unsigned long snake() {
     snakeDirection = 3;
   }
 
-  for (int b = 0; b < 15; b++) {
-    snakeMove(b, snakeDirection);
-  }
-
   boardDirections[snakeBody[0][0]][snakeBody[0][1]] = snakeDirection;
 
-
+  for (int b = 0; b < 15; b++) {
+    snakeMove(b, boardDirections[snakeBody[b][0]][snakeBody[b][1]]);
+  }
 
   snakeRender();
+
+  delay(500);
 }
