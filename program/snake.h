3a4b0cf079654ea312309a24e99f89014b68c609
Brian Gill <work.briangill@gmail.com>
2019-06-04 19:07:25 -0700

    big updates
    stoped working again for some reason? In this: Speed up rendering immensly, but still need to make it re-render the parts that the snake has left, also made snakeMove function to tidy it up a bit

--- program/snake.h
+++ program/snake.h
@@ -1,21 +1,53 @@
-int board [15][20];
-int boardDirections [15][20];
+int board [12][16];
+int boardDirections [12][16];
 int snakeBody [15][2] = { {8, 8}, {8, 9}, {8, 10} };
 
+int firstRender = 1;
 int snakeDirection = 0;
 
 unsigned long snakeRender() {
-  for ( int x = 0; x < 15; x++ ) {
-    for ( int y = 0; y < 20; y++ ) {
-      if (board[x][y] == 0) {
-        tft.fillRect(x*16+1, y*16+1, 14, 14, 65535);
-      } else if ( board[x][y] == 1 ) {
-        tft.fillRect(x*16+1, y*16+1, 14, 14, 61440);
-      } else {
-        tft.fillRect(x*16+1, y*16+1, 14, 14, 2016);
+  if ( firstRender == 1 ) {
+    for ( int x = 0; x < 12; x++ ) {
+      for ( int y = 0; y < 16; y++ ) {
+        if (board[x][y] == 0) {
+          tft.fillRect(x*20+2, y*20+2, 16, 16, 65535);
+        } else if ( board[x][y] == 1 ) {
+          tft.fillRect(x*20+2, y*20+2, 16, 16, 61440);
+        } else {
+          tft.fillRect(x*20+2, y*20+2, 16, 16, 2016);
+        }
       }
     }
+
+    firstRender = 0;
   }
+
+  for ( int p = 0; p < 15; p++ ) {
+    int x = snakeBody[p][0];
+    int y = snakeBody[p][1];
+
+    if (board[x][y] == 0) {
+      tft.fillRect(x*20+2, y*20+2, 16, 16, 65535);
+    } else if ( board[x][y] == 1 ) {
+      tft.fillRect(x*20+2, y*20+2, 16, 16, 61440);
+    } else {
+      tft.fillRect(x*20+2, y*20+2, 16, 16, 2016);
+    }
+  }
+}
+
+unsigned long snakeMove(int a, int d) {
+  board[snakeBody[a][0]][snakeBody[a][1]] = 0;
+  
+  switch( d ) {
+    case 0 : snakeBody[a][1] -= 1;
+    case 1 : snakeBody[a][0] += 1;
+    case 2 : snakeBody[a][1] += 1;
+    case 3 : snakeBody[a][0] -= 1;
+  }
+
+  if ( a == 0 ) { board[snakeBody[a][0]][snakeBody[a][1]] = 1; }
+  else { board[snakeBody[a][0]][snakeBody[a][1]] = 2; }
 }
 
 unsigned long snake() {
@@ -24,47 +56,25 @@ unsigned long snake() {
   int left = analogRead(2);
   int right = analogRead(3);
 
-  Serial.println(snakeDirection);
-
-  board[snakeBody[0][0]][snakeBody[0][1]] = 0;
+  delay(300);
 
   if ( (up == 1022) && (snakeDirection != 2) ) {
-    Serial.println("Up");
     snakeDirection = 0;
   } else if ( (right == 1022) && (snakeDirection != 3) ) {
-    Serial.println("Right");
     snakeDirection = 1;
   } else if ( (down == 1022) && (snakeDirection != 0) ) {
-    Serial.println("Down");
     snakeDirection = 2;
   } else if ( (left == 1022) && (snakeDirection != 1) ) {
-    Serial.println("Left");
     snakeDirection = 3;
+  }
+
+  for (int b = 0; b < 15; b++) {
+    snakeMove(b, snakeDirection);
   }
 
   boardDirections[snakeBody[0][0]][snakeBody[0][1]] = snakeDirection;
 
-  switch( snakeDirection ) {
-    case 0 : snakeBody[0][1] = snakeBody[0][1] - 1; break;
-    case 1 : snakeBody[0][0] = snakeBody[0][0] + 1; break;
-    case 2 : snakeBody[0][1] = snakeBody[0][1] + 1; break;
-    case 3 : snakeBody[0][0] = snakeBody[0][0] - 1; break;
-  }
 
-  board[snakeBody[0][0]][snakeBody[0][1]] = 1;
-
-  for (int b = 1; b < 15; b++) {
-    board[snakeBody[b][0]][snakeBody[b][1]] = 0;
-
-    switch( boardDirections[snakeBody[b][0]][snakeBody[b][1]] ) {
-      case 0 : snakeBody[b][1] = snakeBody[b][1] - 1; break;
-      case 1 : snakeBody[b][0] = snakeBody[b][0] + 1; break;
-      case 2 : snakeBody[b][1] = snakeBody[b][1] + 1; break;
-      case 3 : snakeBody[b][0] = snakeBody[b][0] - 1; break;
-    }
-
-    board[snakeBody[b][0]][snakeBody[b][1]] = 2;
-  }
 
   snakeRender();
 }
