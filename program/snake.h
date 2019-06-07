e1d2c0e67b3491921e85a9f0e283026f03489942
Brian Gill <work.briangill@gmail.com>
2019-06-06 18:51:01 -0700

    use rgb functions, joystick support, reset button, lower delay

--- program/snake.h
+++ program/snake.h
@@ -1,5 +1,5 @@
 int boardDirections [12][16];
-int snakeBody [100][2] = { {8, 8}, {8, 9} };
+int snakeBody [192][2] = { {8, 8}, {8, 9} };
 int food [2] = { rand() % 12, rand() % 16 };
 int newGhost [2];
 int score [3] = { 48, 48, 49 };
@@ -14,24 +14,24 @@ unsigned long gameRender() {
   if ( firstRender == 1 ) {
     for ( int x = 0; x < 12; x++ ) {
       for ( int y = 0; y < 16; y++ ) {
-        tft.fillRect(x*20+2, y*20+2, 16, 16, 65535);
+        tft.fillRect(x*20+2, y*20+2, 16, 16, rgb(255, 255, 255));
       }
     }
 
     firstRender = 0;
   }
 
-  tft.fillRect(snakeBody[0][0]*20+2, snakeBody[0][1]*20+2, 16, 16, 61440);
+  tft.fillRect(snakeBody[0][0]*20+2, snakeBody[0][1]*20+2, 16, 16, rgb(255, 0, 0));
 
   for ( int p = 1; p < snakeLength; p++ ) {
     int x = snakeBody[p][0];
     int y = snakeBody[p][1];
     
-    tft.fillRect(x*20+2, y*20+2, 16, 16, 2016);
+    tft.fillRect(x*20+2, y*20+2, 16, 16, rgb(0, 255, 0));
   }
 
-  tft.fillRect(snakeBody[snakeLength][0]*20+2, snakeBody[snakeLength][1]*20+2, 16, 16, 65535);
-  tft.fillRect(food[0]*20+2, food[1]*20+2, 16, 16, 0);
+  tft.fillRect(snakeBody[snakeLength][0]*20+2, snakeBody[snakeLength][1]*20+2, 16, 16, rgb(255, 255, 255));
+  tft.fillRect(food[0]*20+2, food[1]*20+2, 16, 16, rgb(0, 0, 0));
 }
 
 unsigned long snakeMove(int a, int d) {
@@ -82,19 +82,40 @@ unsigned long checkGame() {
 }
 
 unsigned long snake() {
-  if ( gameOver == 0 ) {
-    int up = analogRead(0);
-    int down = analogRead(1);
-    int left = analogRead(2);
-    int right = analogRead(3);
+  int rs = analogRead(2);
+  if (rs == 1022) {
+    tft.fillScreen(0);
+    snakeBody[0][0] = 8;
+    snakeBody[0][1] = 8;
+    snakeBody[1][0] = 8;
+    snakeBody[1][1] = 9;
+    score[0] = 48;
+    score[1] = 48;
+    score[2] = 49;
+    for (int n = 2; n < snakeLength; n++) {
+      snakeBody[n][0] = 0;
+      snakeBody[n][1] = 0;
+      boardDirections[n][0] = 0;
+      boardDirections[n][1] = 0;
+    }
+    firstRender = 1;
+    snakeDirection = 0;
+    snakeLength = 1;
+    gameOver = 0;
+    ateFood = 0;
+  }
 
-    if ( (up == 1022) && (snakeDirection != 2) ) {
+  if ( gameOver == 0 ) {
+    int dx = analogRead(0)-514;
+    int dy = analogRead(1)-516;
+
+    if ( (dy < -200) && (snakeDirection != 2) ) {
       snakeDirection = 0;
-    } else if ( (right == 1022) && (snakeDirection != 3) ) {
+    } else if ( (dx > 200) && (snakeDirection != 3) ) {
       snakeDirection = 1;
-    } else if ( (down == 1022) && (snakeDirection != 0) ) {
+    } else if ( (dy > 200) && (snakeDirection != 0) ) {
       snakeDirection = 2;
-    } else if ( (left == 1022) && (snakeDirection != 1) ) {
+    } else if ( (dx < -200) && (snakeDirection != 1) ) {
       snakeDirection = 3;
     }
 
@@ -114,14 +135,12 @@ unsigned long snake() {
 
     checkGame();
 
-    delay(500);
+    delay(300);
   } else {
     tft.fillScreen(61440);
 
     tft.drawChar(15, 100, score[0], 0, 65535, 12);
     tft.drawChar(85, 100, score[1], 0, 65535, 12);
     tft.drawChar(155, 100, score[2], 0, 65535, 12);
-
-    delay(100000);
   }
 }
